a0ec9cea6e5ba109f18b7e5ac6fc0befc247f945
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-11 00:48:39 +0300

    Feat(JS) Complete 3 task

--- js/task-03.js
+++ js/task-03.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (!emailInput.value || !passwordInput.value) {
+    alert('Усі поля повинні бути заповнені');
+    return;
+  }
+
+  const loginData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(loginData);
+
+  loginForm.reset();
+});
