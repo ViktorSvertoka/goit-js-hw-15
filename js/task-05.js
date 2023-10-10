60ff8302180d941a61694c50d3eb2fe2424ac041
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-11 01:44:36 +0300

    Feat(JS) Complete 5 task

--- js/task-05.js
+++ js/task-05.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const input = document.querySelector('input');
+const createButton = document.querySelector('[data-create]');
+const destroyButton = document.querySelector('[data-destroy]');
+const boxesDiv = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const div = document.createElement('div');
+    const size = 30 + i * 10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.background = getRandomHexColor();
+    boxesDiv.insertAdjacentElement('beforeend', div);
+  }
+}
+
+createButton.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+  input.value = '';
+}
+
+destroyButton.addEventListener('click', destroyBoxes);
