05fa608dcf98ed49b3887d5f679b4a7a88d07f97
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-11 00:31:22 +0300

    Feat(JS) Complete 2 task

--- js/task-02.js
+++ js/task-02.js
@@ -1,1 +1,10 @@
+const rangeInput = document.getElementById('font-size-control');
+const onText = document.getElementById('text');
 
+onText.style.fontSize = rangeInput.value + 'px';
+
+const textResizing = () => {
+  onText.style.fontSize = `${rangeInput.value}px`;
+};
+
+rangeInput.addEventListener('input', textResizing);
