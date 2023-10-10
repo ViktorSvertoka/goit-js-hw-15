0de4eee50ed9c5ed129ad99a926bdea722dec0d3
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-10-11 01:07:24 +0300

    Feat(JS) Complete 4 task

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
 
+const button = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.background = randomColor;
+  span.textContent = randomColor;
+});
